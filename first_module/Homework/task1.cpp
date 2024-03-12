348c6f2544be6e19a028c5df0aa7e02cc8ca37e6
Alexeyzem <Alex.zem2015@mail.ru>
2024-03-12 09:05:03 +0300

    update codition for tasks

--- first_module/Homework/task1.cpp
+++ first_module/Homework/task1.cpp
@@ -1,7 +1,18 @@
+// Инвертируйте значение бита в числе N по его номеру K
+
 #include "iostream"
 
+unsigned int inverseBit(int data, int pos){
+    int tmp = 1 << pos;
+    return data xor tmp;
+}
+
 int main(){
-
-
+    unsigned int N = 0;
+    std::cin >> N;
+    int k = 0;
+    std::cin >> k;
+    unsigned int out = inverseBit(N, k);
+    std::cout << out;
     return 0;
 }
